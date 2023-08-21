7f917d0eb7efb8d90ab2b04244ed4c00853550c2
张文皓 <zhangwh754@163.com>
2023-08-21 15:05:30 +0800

    🎉 init: 初始化

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+  await app.listen(3000)
+  console.log(`实例启动于： localhost:${3000}`)
+}
+bootstrap()
