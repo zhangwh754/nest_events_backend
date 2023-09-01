e2c211861e2cec8fcd806190035a5832182a735e
张文皓 <zhangwh754@163.com>
2023-09-01 10:22:41 +0800

    ✨ feat: remove module do not need(dynamic module)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,24 +2,13 @@ import { Module, Logger } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { ConfigModule } from '@nestjs/config'
 
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { EventsModule } from './events/events.module'
-import { AppChineseService } from './app.chinese.service'
 import { ormConfig, ormConfigProd } from './app/config'
 import { AttendeeModule } from './attendee/attendee.module'
 import { SchoolModule } from './school/school.module'
 import { ArticleModule } from './article/article.module'
 import { UserModule } from './user/user.module'
 import { AuthModule } from './auth/auth.module'
-
-const isChinese = true
-
-class AppDummy {
-  public getDummy(): string {
-    return 'dummy msg'
-  }
-}
 
 @Module({
   imports: [
@@ -37,23 +26,7 @@ class AppDummy {
     UserModule,
     AuthModule,
   ],
-  controllers: [AppController],
-  providers: [
-    Logger,
-    {
-      provide: AppService,
-      useClass: isChinese ? AppChineseService : AppService,
-    },
-    {
-      provide: 'APP_NAME',
-      useValue: 'Nest Events Backend',
-    },
-    AppDummy,
-    {
-      provide: 'MESSAGE',
-      inject: [AppDummy],
-      useFactory: app => app.getDummy(),
-    },
-  ],
+  controllers: [],
+  providers: [Logger],
 })
 export class AppModule {}
